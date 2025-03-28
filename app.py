551c48b189bc2527b7906a9d38a4531a33808eb0
Israel-TecP <cesarisraelmorenoflores1@gmail.com>
2025-03-27 22:30:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template ("index.html")
+
+@app.route("/Agregar")
+def Agregar_Clientes():
+    return render_template ("Agregar_Clientes.html")
